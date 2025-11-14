1e47bacfd91a4e1d59c80dc58bb23af3037c0b3f
guantino <guantino@student.42malaga.com>
2025-11-14 18:46:08 +0100

    a

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guantino <guantino@student.42malaga.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/14 18:21:50 by guantino          #+#    #+#             */
+/*   Updated: 2025/11/14 18:27:53 by guantino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <stdlib.h>
+#include <fcntl.h>
+
+char	*get_next_line(int fd);
+
+#endif
